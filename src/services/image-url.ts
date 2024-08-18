0d81b4c5f957b1feef4639d480c1258f752d6176
yanedie <zinyi073@gmail.com>
2024-08-18 22:52:25 +0800

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,8 @@
+// 使用本地图片不能直接输入路径，而是要用导入的方式
+import noImagge from "../assets/no-image-placeholder.webp";
+
 export default function getCroppedImageUrl(url: string) {
-  if (!url) return "";
+  if (!url) return noImagge;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
