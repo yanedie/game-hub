02c5982b544b1dd9bd337bf1bb31f52c1793243d
yanedie <zinyi073@gmail.com>
2024-09-12 00:31:02 +0800

    Clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,23 +4,25 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 interface Props {
-  game: Game;
+    game: Game;
 }
 const GameCard = ({ game }: Props) => {
-  return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map(({ platform }) => platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
-  );
+    return (
+        <Card>
+            <Image src={getCroppedImageUrl(game.background_image)} />
+            <CardBody>
+                <HStack justifyContent={"space-between"} marginBottom={3}>
+                    <PlatformIconList
+                        platforms={game.parent_platforms.map(
+                            ({ platform }) => platform
+                        )}
+                    />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
+                <Heading fontSize="2xl">{game.name}</Heading>
+            </CardBody>
+        </Card>
+    );
 };
 
 export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,36 +7,36 @@ import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
 
 interface Props {
-  gameQuery: GameQuery;
+    gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6];
-  // Array.from({ length: data.length + 1 }, (_, i) => i++);
+    const { data, error, isLoading } = useGames(gameQuery);
+    const skeletons = [1, 2, 3, 4, 5, 6];
+    // Array.from({ length: data.length + 1 }, (_, i) => i++);
 
-  return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={3}
-      >
-        {isLoading &&
-          skeletons.map((Skeleton) => (
-            <GameCardContainer key={Skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
-      </SimpleGrid>
-    </>
-  );
+    return (
+        <>
+            {error && <Text>{error}</Text>}
+            <SimpleGrid
+                columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+                padding="10px"
+                spacing={6}
+            >
+                {isLoading &&
+                    skeletons.map((Skeleton) => (
+                        <GameCardContainer key={Skeleton}>
+                            <GameCardSkeleton />
+                        </GameCardContainer>
+                    ))}
+                {data.map((game) => (
+                    <GameCardContainer key={game.id}>
+                        <GameCard game={game} />
+                    </GameCardContainer>
+                ))}
+            </SimpleGrid>
+        </>
+    );
 };
 
 export default GameGrid;
