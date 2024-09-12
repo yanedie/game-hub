d877d2076b832ad1d964ede6b04f92425fbd9dcf
yanedie <zinyi073@gmail.com>
2024-09-13 00:49:50 +0800

    Fix typo

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,7 +3,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
-import Emoji from "./emoji";
+import Emoji from "./Emoji";
 interface Props {
     game: Game;
 }
